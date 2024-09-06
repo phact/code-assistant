54e28733507aeb00ec2127a6bd4d48b550b86350
phact <estevezsebastian@gmail.com>
2024-09-06 13:41:59 -0400

    chat_message page bug fix

--- code_assistant/routes/chat_message.py
+++ code_assistant/routes/chat_message.py
@@ -2,5 +2,7 @@ from code_assistant.app import *
 
 # Route that gets polled while streaming
 async def page(request, msg_idx: int):
-    if msg_idx >= len(request.state.messages): return ""
+    if not hasattr(request.app.state, 'messages'):
+        return Script("window.location.href = '/';")
+    if msg_idx >= len(request.app.state.messages): return ""
     return ChatMessage(request.app.state.messages, msg_idx)
