2e5874b673dcef8db0776e93a2e7e878142144e9
phact <estevezsebastian@gmail.com>
2024-09-10 11:32:59 -0400

    feat: Add small_fasthtml_context to constants.context package

--- code_assistant/assistants.py
+++ code_assistant/assistants.py
@@ -15,7 +15,7 @@ from code_assistant.constants.config import GENERATED_APPS_DIR
 from openai import OpenAI, BaseModel
 from pydantic import Field
 
-from code_assistant.constants.small_fasthtml_context import small_fasthtml_context
+from code_assistant.constants.context.small_fasthtml_context import small_fasthtml_context
 from code_assistant.util.file_util import get_mount_from_file
 
 
